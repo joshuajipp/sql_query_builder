8246014a06a89d2822eec752f68fb90d56f4e406
Joshua Jipp <joshuajipp@gmail.com>
2022-11-26 08:58:32 -0700

    creating the layout/design for the class

---
+++ queryClass.py
@@ -0,0 +1,21 @@
+class QueryBuilder():
+    def __init__(self, database):
+        self.database = database
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    def create(self):
+        pass
+
+    def alter(self):
+        pass
+
+    def query(self):
+        pass
