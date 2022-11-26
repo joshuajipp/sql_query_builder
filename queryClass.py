623114baa8f96deb9da51f276f05d4a6f5380892
Joshua Jipp <joshuajipp@gmail.com>
2022-11-26 16:00:56 -0700

    created columnInfo class method and finished delete method implementation

--- queryClass.py
+++ queryClass.py
@@ -4,12 +4,12 @@ import mysql.connector
 class QueryBuilder():
     def __init__(self, username, pword, db, table):
         try:
-            tmp = mysql.connector.connect(
+            self.data = mysql.connector.connect(
                 host="127.0.0.1", port=3306, user=username, password=pword, database=db)
-            self.db = tmp.cursor()
-            self.db.execute(f"SELECT * FROM {table}")
+            tmp = self.data.cursor(buffered=True)
+            tmp.execute(f"SELECT * FROM {table}")
         except Exception as error:
-            if "Unknown database" in str(error):
+            if f"Unknown database '{db}'" in str(error):
                 raise ValueError("Must enter valid database")
                 # raise ValueError("Invalid database")
             elif f"Table '{db}.{table}' doesn't exist" in str(error):
@@ -22,8 +22,10 @@ class QueryBuilder():
     def insert(self):
         pass
 
-    def delete(self):
-        pass
+    def delete(self, condition):
+        tmp = self.data.cursor(buffered=True)
+        tmp.execute(f"DELETE FROM {self.table} WHERE {condition}")
+        self.data.commit()
 
     def update(self):
         pass
@@ -33,3 +35,10 @@ class QueryBuilder():
 
     def query(self):
         pass
+
+    def columnInfo(self):
+        tmp = self.data.cursor(buffered=True)
+        tmp.execute(f"SELECT * FROM {self.table}")
+        column_nullable_dict = {i[0]: i[6] for i in tmp.description}
+
+        return column_nullable_dict
