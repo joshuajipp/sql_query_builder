db33f8e8e15b60a3025ec22a5da2c887c012f36a
Joshua Jipp <joshuajipp@gmail.com>
2022-11-27 00:34:50 -0700

    add insert function

--- main.py
+++ main.py
@@ -16,34 +16,36 @@ def main():
 
     option = input(
         "a.\tInsert\nb.\tDelete\nc.\tUpdate\nd.\tCreate Table\ne.\t Create view\nf.\tAlter\ng.\tQuery\nh.\tEXIT\nEnter letter to choose operation: ")
-    
+
     valid_operation = validateOperation(option)
 
     while not valid_operation:
         option = input(
-        "a.\tInsert\nb.\tDelete\nc.\tUpdate\nd.\tCreate Table\ne.\t Create view\nf.\tAlter\ng.\tQuery\nh.\tEXIT\nEnter letter to choose operation: ")
-        valid_operation = validateOperation(option) 
-    
+            "a.\tInsert\nb.\tDelete\nc.\tUpdate\nd.\tCreate Table\ne.\t Create view\nf.\tAlter\ng.\tQuery\nh.\tEXIT\nEnter letter to choose operation: ")
+        valid_operation = validateOperation(option)
+
     if valid_operation == "a":
+        tables = QueryBuilder(user, password, schema, 'country').getTableNames
+        print(f"Tables in {schema}: {tables}")
         table_name = input("Table you want to insert data to:")
 
     if valid_operation == "b":
         table_name = input("Table to delete from: ")
-    
+
     if valid_operation == "c":
         table_name = input("Table to update: ")
-    
+
     if valid_operation == "d":
         table_name = input("Table name: ")
         num_attributes = int(input("Enter the number of attributes: "))
         i = 0
         while i < num_attributes:
-            #ask for attribute name
-            #data type
-    
+            # ask for attribute name
+            # data type
+
     if valid_operation == "e":
         table_name = input("Table ")
-    
+
     if valid_operation == "f":
         table_name = input("Table ")
 
