8cb9471bc24f7d2669cb454179cbfd51cf3953b1
Joshua Jipp <joshuajipp@gmail.com>
2022-11-26 18:53:03 -0700

    added input menu for user

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import mysql.connector
 from queryClass import *
 from validate import *
+from getpass import getpass
 
 
 def main():
     user = input("Username: ")
-    password = input("Password: ")
-    valid_credentials = validateCredentials(user, password)
+    password = getpass("Password: ")
+    valid_credentials = True
 
     while not valid_credentials:
         user = input("Username: ")
-        password = input("Password: ")
+        password = getpass("Password: ")
         valid_credentials = validateCredentials(user, password)
+
+    option = input(
+        "a.\tInsert\nb.\tDelete\nc.\tUpdate\nd.\tCreate Table\ne.\t Create view\nf.\tAlter\ng.\tQuery\nEnter letter to choose operation: ")
 
 
 if __name__ == "__main__":
