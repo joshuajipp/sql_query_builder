0c6a122832e5585e848514b47e0893c02062a341
Joshua Jipp <joshuajipp@gmail.com>
2022-11-26 19:45:27 -0700

    finished implemetning class methods

--- queryClass.py
+++ queryClass.py
@@ -27,16 +27,45 @@ class QueryBuilder():
         tmp.execute(f"DELETE FROM {self.table} WHERE {condition}")
         self.data.commit()
 
-    def update(self):
-        pass
+    def update(self, set, condition):
+        tmp = self.data.cursor(buffered=True)
+        tmp.execute(f"UPDATE {self.table} SET {set} WHERE {condition}")
+        self.data.commit()
 
-    def alter(self):
-        pass
+    def addModify(self, action, column_name, data_type):
+        tmp = self.data.cursor(buffered=True)
 
-    def query(self):
-        pass
+        col_info = self.__columnInfo()
 
-    def columnInfo(self):
+        if action.lower() == "modify":
+            if not (column_name in col_info):
+                raise ValueError("Invalid column name")
+
+        tmp.execute(
+            f"ALTER TABLE {self.table} {action} COLUMN {column_name} {data_type}")
+        self.data.commit()
+
+    def drop(self, column_name):
+        tmp = self.data.cursor(buffered=True)
+        col_info = self.__columnInfo()
+
+        if not (column_name in col_info):
+            raise ValueError("Invalid column name")
+
+        tmp.execute(
+            f"ALTER TABLE {self.table} DROP COLUMN {column_name}")
+        self.data.commit()
+
+    def query(self, columns, condition=None, order_by=None):
+        tmp = self.data.cursor(buffered=True)
+        select_str = f"SELECT {columns} FROM {self.table}"
+        if condition != None:
+            select_str += f"WHERE {condition}"
+        if order_by != None:
+            select_str += f"ORDER BY {order_by}"
+        tmp.execute(select_str)
+
+    def __columnInfo(self):
         tmp = self.data.cursor(buffered=True)
         tmp.execute(f"SELECT * FROM {self.table}")
         column_nullable_dict = {i[0]: i[6] for i in tmp.description}
