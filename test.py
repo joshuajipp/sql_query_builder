5e53885b329aa28eb098e204ec888d34558314fa
mizybermas <mizy.bermas@ucalgary.ca>
2022-11-30 23:21:27 -0700

    function to format data to table

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import mysql.connector
 from queryClass import *
+from displayData import *
 
 cnx = mysql.connector.connect(
-    host="127.0.0.1", port=3306, user="root", password="R6clolvalrr.#")
+    host="127.0.0.1", port=3306, user="root", password="Mx.ze0218")
 
 
-obj = QueryBuilder("root", "R6clolvalrr.#", "olympicarchery", 'country')
+#obj = QueryBuilder("root", "R6clolvalrr.#", "olympicarchery", 'country')
 
-obj.drop("AllTimeDubs")
+#obj.drop("AllTimeDubs")
 # print(cur.fetchall())
 # table = "olympicarchery.coach"
 
 # columns = [i[0] for i in cur.description]
 
 # obj = QueryBuilder(cnx, table)
-
-
+column =  ['name', 'kids']
+row = [('gerry', 3),('lingling', 1),('rowena',3)]
+displayTable(column, row)
