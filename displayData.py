02ba78e0b9fd7cbb83c8702b4051e3f13512d28a
mizybermas <mizy.bermas@ucalgary.ca>
2022-12-01 23:06:56 -0700

    fixed displayTable.py

--- displayData.py
+++ displayData.py
@@ -1,13 +1,5 @@
 def displayTable(column_names, rows_list):
-    longest_instance = {}
-    for i in column_names:
-        longest_instance[i] = len(i)
-
-    for i in rows_list:
-         for j in range(len(i)):
-            if len(i[j]) > longest_instance[column_names[j]]:
-                longest_instance[column_names[j]] = len(i[j])
-
+    
     header_size = len(column_names)
     for i in range(header_size):
         print("{:<15s}".format(column_names[i]),end='')
